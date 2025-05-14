4b7a1cf57748136c5c902e452f8b9d0590330bdd
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-05-14 12:10:09 +0300

    use router

--- Server/api.js
+++ Server/api.js
@@ -7,9 +7,13 @@ app.use(express.json());
 
 const apiEmailsRouter = require("./routers/emails");
 app.use("/emails", apiEmailsRouter);
+const apiDraftsRouter = require("./routers/drafts");
+app.use("/drafts", apiDraftsRouter);
+const apiUsersRouter = require("./routers/users");
+app.use("/users", apiUsersRouter);
 
 app.get("/", (req, res) => {
-  console.log("Request received!");  
+  console.log("Request received!");
   res.send("Connected to the database and server");
 });
 
