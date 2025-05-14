50ec7939e1fab1affc4d7a78b49661459917aba7
שרה תמר אמיתי <saratama@g.jct.ac.il>
2025-05-14 12:28:54 +0300

    users router

---
+++ Server/routers/users.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const router = express.Router();
+
+const url = "mongodb://localhost:27017/";
+const client = new MongoClient(url);
+let dbo;
+
+client
+  .connect()
+  .then(() => {
+    dbo = client.db("email");
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await dbo.collection("users").findOne({ email, password });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    res.json({
+      message: "Login successful",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Database error", error: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { email, name } = req.body;
+  const newUser = {
+    email,
+    name,
+  };
+
+  try {
+    const existingUser = await dbo.collection("users").findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "email address already exists" });
+    }
+    await dbo.collection("users").insertOne(newUser);
+    res.json({
+      message: "the user was added succsessfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Database error", error: error.message });
+  }
+});
+
+module.exports = router;
